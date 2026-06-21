eac2ae3e598e57aa55d8fb5056f33a99f61671b5
RahulKiran.G <rahulkiran2222@gmail.com>
2026-06-21 21:31:11 +0530

    Update and rename app.py to model.py

---
+++ model.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+class TinyGPT(nn.Module):
+    def __init__(self, vocab_size, n_embd=256, n_head=8, n_layer=4, block_size=256):
+        super().__init__()
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.blocks = nn.Sequential(*[Block(n_embd, n_head, block_size) for _ in range(n_layer)])
+        self.ln_f = nn.LayerNorm(n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
+        self.block_size = block_size
+
+    def forward(self, idx, targets=None):
+        B, T = idx.shape
+        tok_emb = self.token_embedding_table(idx)
+        pos_emb = self.position_embedding_table(torch.arange(T, device=idx.device))
+        x = tok_emb + pos_emb
+        x = self.blocks(x)
+        x = self.ln_f(x)
+        logits = self.lm_head(x)
+
+        if targets is None:
+            loss = None
+        else:
+            B, T, C = logits.shape
+            logits = logits.view(B*T, C)
+            targets = targets.view(B*T)
+            loss = F.cross_entropy(logits, targets)
+
+        return logits, loss
+
+class Block(nn.Module):
+    def __init__(self, n_embd, n_head, block_size):
+        super().__init__()
+        head_size = n_embd // n_head
+        self.sa = nn.MultiheadAttention(n_embd, n_head, batch_first=True)
+        self.ffwd = nn.Sequential(
+            nn.Linear(n_embd, 4 * n_embd),
+            nn.ReLU(),
+            nn.Linear(4 * n_embd, n_embd),
+        )
+        self.ln1 = nn.LayerNorm(n_embd)
+        self.ln2 = nn.LayerNorm(n_embd)
+
+    def forward(self, x):
+        attn_output, _ = self.sa(self.ln1(x), self.ln1(x), self.ln1(x))
+        x = x + attn_output
+        x = x + self.ffwd(self.ln2(x))
+        return x
